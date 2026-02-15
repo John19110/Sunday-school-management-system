67f7cab0578f1a28e52010311858e763f20e9d8d
John19110 <2022170119@cis.asu.edu.eg>
2026-02-15 23:25:27 +0200

    Daily updates

--- SunDaySchoolsDAL/Models/Exams.cs
+++ SunDaySchoolsDAL/Models/Exams.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SunDaySchools.Models;
 
 namespace SunDaySchools.DAL.Models
 {
     public class Exams
     {
         public int Id { get; set; }
+
+        public List<Child> Children { get; set; }
+
+        public DateOnly ExamDate { get; set; }
+
+        public List<Child> TopChilds { get; set; }
+
+        public string   Notes { get; set; }
+
 
 
 
